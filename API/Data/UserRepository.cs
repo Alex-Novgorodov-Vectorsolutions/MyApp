e621cb2a43644236859edf4ef531b67b0161f5e5
AlexN_0276 <alexander.novgorodov@vectorsolutions.com>
2020-12-14 19:29:07 -0500

    session 8

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -23,16 +23,16 @@ namespace API.Data
         public async Task<MemberDto> GetMemberAsync(string username)
         {
             return await _context.Users
-            .Where(x => x.UserName == username)
-            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-            .SingleOrDefaultAsync();
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
         }
 
         public async Task<IEnumerable<MemberDto>> GetMembersAsync()
         {
-            return await _context.Users
-            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-            .ToListAsync();
+            return await _context.Users 
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -43,18 +43,18 @@ namespace API.Data
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
             return await _context.Users
-            .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == username);
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
             return await _context.Users
-            .Include(p => p.Photos)
-            .ToListAsync();
+                .Include(p => p.Photos)
+                .ToListAsync();
         }
 
-        public async Task<bool> SaveAllAsynch()
+        public async Task<bool> SaveAllAsync()
         {
             return await _context.SaveChangesAsync() > 0;
         }
@@ -64,4 +64,4 @@ namespace API.Data
             _context.Entry(user).State = EntityState.Modified;
         }
     }
-}
+} 
