30e520eba8b1ea7834814027c2e29904525fb3d6
AlexNovgorodov <alexander.novgorodov@vectorsolutions.com>
2021-01-05 23:52:34 -0500

    lesson 150

--- client/src/app/_forms/date-input/date-input.component.ts
+++ client/src/app/_forms/date-input/date-input.component.ts
@@ -8,25 +8,24 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
   styleUrls: ['./date-input.component.css']
 })
 export class DateInputComponent implements ControlValueAccessor {
-@Input() label: string;
-@Input() maxDate: Date;
-bsConfig: Partial<BsDatepickerConfig>;
+  @Input() label: string;
+  @Input() maxDate: Date;
+  bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(@Self() public ngControl: NgControl) {
+  constructor(@Self() public ngControl: NgControl) { 
     this.ngControl.valueAccessor = this;
     this.bsConfig = {
       containerClass: 'theme-red',
       dateInputFormat: 'DD MMMM YYYY'
     }
-   }
+  }
 
   writeValue(obj: any): void {
   }
+
   registerOnChange(fn: any): void {
   }
+
   registerOnTouched(fn: any): void {
   }
-
-
-
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,17 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
-  constructor(private accountService: AccountService, private toastr: ToastrService,
-     private fb: FormBuilder) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, 
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.intitializeForm();
     this.maxDate = new Date();
-    this.maxDate.setFullYear(this.maxDate.getFullYear() -18)
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
   }
 
   intitializeForm() {
@@ -37,25 +38,23 @@ export class RegisterComponent implements OnInit {
     })
   }
 
-  matchValues(matchTo: string ): ValidatorFn{
+  matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control?.value === control?.parent?.controls[matchTo].value?
-       null: {isMatching: true}
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true}
     }
   }
 
-  register(){
-    console.log(this.registerForm.value);
-  /*  this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
+  register() {
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
-      this.toastr.error(error.error);    
-    })*/
+      this.validationErrors = error;
+    })
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
   }
+
 }
