2d3230a2334ffafde5ab85e9ea5588e2c8ed25b1
AlexNovgorodov <alexander.novgorodov@vectorsolutions.com>
2021-02-24 16:29:42 -0500

    lesson 176

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from 'src/app/_services/members.service';
+
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]>;
+  predicate = 'liked';
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe((response => {
+      this.members = response;
+    }))
   }
 
 }
